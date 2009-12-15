c449930bd95c4dbf8af430fcfe71801c01ad2828
vady <vady@MacBook-Pro.local>
2009-12-15 22:56:20 +0100

    Project structure

---
+++ src/main/java/com/vady/da4124/App.java
@@ -0,0 +1,13 @@
+package com.vady.da4124;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
