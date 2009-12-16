33be0ea1d61011bf89ea96a2f01bacce2d0e85f1
vady <vady@MacBook-Pro.local>
2009-12-16 01:28:58 +0100

    More GraphMLParser tests

--- src/test/java/com/vady/da4124/AppTest.java
+++ src/test/java/com/vady/da4124/AppTest.java
@@ -1,10 +1,14 @@
 package com.vady.da4124;
 
 import com.vady.da4124.parser.GraphMLParser;
+import com.sun.deploy.security.KeychainCertificateStore;
 import grail.interfaces.DirectedGraphInterface;
 import grail.interfaces.NodeInterface;
+import grail.interfaces.EdgeInterface;
+import grail.interfaces.DirectedEdgeInterface;
 import grail.properties.GraphProperties;
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import java.io.File;
@@ -14,7 +18,7 @@ import java.util.List;
 public class AppTest {
 
     @Test
-    public void testParser() {
+    public void testParserOnGraph10() {
         File file = new File("g.10.1.graphml");
 
         GraphMLParser graphMLParser = new GraphMLParser();
@@ -36,7 +40,46 @@ public class AppTest {
         assertEquals("n6", node.getProperty(GraphProperties.LABEL));
 
         node = graph.getNode("EGZ");
-        assertNull(node);       
+        assertNull(node);
+    }
+
+    @Test
+    public void testParserOnGraph100() {
+        File file = new File("g.100.10.graphml");
+
+
+        GraphMLParser graphMLParser = new GraphMLParser();
+        List graphs = graphMLParser.load(file);
+
+        assertNotNull(graphs);
+        assertFalse(graphs.isEmpty());
+        assertEquals(1, graphs.size());
+
+        DirectedGraphInterface graph = (DirectedGraphInterface) graphs.get(0);
+
+        NodeInterface node = graph.getNode("EGZ");
+        assertNull(node);
+
+        assertEquals(100, graph.nodeCount());
+        assertEquals(163, graph.edgeCount());
+
+        NodeInterface node1 = graph.getNode("n39");
+        assertNotNull(node1);
+
+        assertEquals("n39", node1.getKey());
+        assertEquals("n39", node1.getProperty(GraphProperties.LABEL));
+
+        NodeInterface node2 = graph.getNode("n99");
+        assertNotNull(node1);
+
+        assertEquals("n99", node2.getKey());
+        assertEquals("n99", node2.getProperty(GraphProperties.LABEL));
+
+        DirectedEdgeInterface edge = (DirectedEdgeInterface) graph.getEdge("e162");
+        assertNotNull(edge);
+
+        assertEquals(node1, edge.getFrom());
+        assertEquals(node2, edge.getTo());
     }
 
 }
